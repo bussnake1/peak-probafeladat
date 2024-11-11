025ca725e60335ea02a4d3c2e420835e9fcb9895
Daniel Pocs <bussnake@Daniels-MacBook-Air.local>
2024-11-11 17:36:00 +0100

    added detailed view

---
+++ src/app/stock/[symbol]/page.tsx
@@ -0,0 +1,33 @@
+import Link from 'next/link'
+import { StockDetails } from '@/components/ui/StockDetails'
+import { notFound } from 'next/navigation'
+
+type Props = {
+  params: {
+    symbol: string
+  }
+}
+
+export default function StockDetailPage({ params }: Props) {
+  const { symbol } = params
+
+  if (!symbol) {
+    notFound()
+  }
+
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <div className="w-full max-w-5xl">
+        <div className="mb-6">
+          <Link 
+            href="/" 
+            className="text-primary hover:text-primary/80 flex items-center"
+          >
+            ← Back to Search
+          </Link>
+        </div>
+        <StockDetails symbol={symbol} />
+      </div>
+    </main>
+  )
+}
